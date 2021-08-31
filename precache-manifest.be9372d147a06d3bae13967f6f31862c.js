8e3f8307903bfce97e78b4a641264e0d1c2fd50b
Igor <2492345@gmail.com>
2021-08-31 20:57:20 +0300

    Updates

---
+++ precache-manifest.be9372d147a06d3bae13967f6f31862c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce1d893369c8b8febc67226d9500882",
+    "url": "./index.html"
+  },
+  {
+    "revision": "482e96c52ac42368c5ea",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482e96c52ac42368c5ea",
+    "url": "./static/js/main.6d98f2d5.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
